da99e5332ab767e9de05529f1b4778f05234da4e
ib00sh <wowmaxwow98@gmail.com>
2018-08-06 05:53:31 +0300

    Добавлен класс для вывода алфавита. Использована конструкция вида (char)('A'+i)

---
+++ src/AlphaBet/AlphabetDemo.java
@@ -0,0 +1,9 @@
+package AlphaBet;
+
+public class AlphabetDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 26 ; i++) {
+            System.out.print((char)('A' + i));
+        }
+    }
+}
